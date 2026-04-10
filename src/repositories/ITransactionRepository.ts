d939c563da676432da3585ee3d8e8be92576af50
Kundan-CR7 <Kundangupta7334@gmail.com>
2026-04-10 23:20:25 +0530

    proper initialization ts and prisma

--- src/repositories/ITransactionRepository.ts
+++ src/repositories/ITransactionRepository.ts
@@ -3,4 +3,5 @@ import { Transaction } from "../domain/Transaction";
 export interface ITransactionRepository{
     save(transaction: Transaction): Promise<void>
     findById(id: string): Promise<Transaction | null>
+    findByStatementId(statementId: string): Promise<Transaction[]>
 }
