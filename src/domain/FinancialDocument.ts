4a8c777c231b522106ecc07a892b7964796e6bc6
Kundan-CR7 <Kundangupta7334@gmail.com>
2026-04-14 16:33:26 +0530

    implement InvoiceItem logic

---
+++ src/domain/FinancialDocument.ts
@@ -0,0 +1,25 @@
+import { stat } from "node:fs"
+
+export type DocumentStatus = "PENDING" | "VERIFIED" | "FLAGGED" | "RECONCILED"
+
+export abstract class FinancialDocument{
+    protected readonly id: string
+    protected status: DocumentStatus
+    protected readonly createdAt: Date
+
+    constructor(id: string, status: DocumentStatus="PENDING",createdAt?: Date){
+        this.id = id
+        this.status = status
+        this.createdAt = createdAt || new Date()
+    }
+
+    public getId(): string { return this.id; }
+    public getStatus(): DocumentStatus { return this.status; }
+    public getCreatedAt(): Date { return this.createdAt; }
+    
+    public flagAsAnomaly():void {
+        this.status = "FLAGGED"
+    }
+
+    public abstract process(): void
+}
---
+++ src/domain/InvoiceItem.ts
@@ -0,0 +1,29 @@
+export class InvoiceItem{
+    private readonly id: string
+    private readonly description: string
+    private readonly hsnCode: string
+    private readonly amount: number
+    private readonly gstRate: number     //Stored as a percentage(eg 18 for 18%)
+
+    constructor(id:string, description:string, hsnCode: string, amount: number, gstRate: number){
+        if(amount < 0){
+            throw new Error("Invoice item cannot be negative.")
+        }
+        if(gstRate < 0 || gstRate > 100){
+            throw new Error("Invalid GST rate");
+        }
+        this.id = id
+        this.description = description
+        this.hsnCode = hsnCode
+        this.amount = amount
+        this.gstRate = gstRate
+    } 
+
+    public calculateTaxAmount(): number {
+        return this.amount * (this.gstRate / 100)
+    }
+
+    public getAmount(): number { return this.amount; }
+    public getGstRate(): number { return this.gstRate; }
+    public getHsnCode(): string { return this.hsnCode; }
+}
